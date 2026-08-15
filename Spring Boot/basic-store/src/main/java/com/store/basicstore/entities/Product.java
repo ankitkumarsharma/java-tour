418fc04d00f339a3a870e2a11839207345065e60
ankitkumarsharma <aksharma.dixit@gmail.com>
2026-08-16 00:34:29 +0530

    added more validations on product

--- Spring Boot/basic-store/src/main/java/com/store/basicstore/entities/Product.java
+++ Spring Boot/basic-store/src/main/java/com/store/basicstore/entities/Product.java
@@ -2,6 +2,10 @@ package com.store.basicstore.entities;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.*;
 
 import java.math.BigDecimal;
@@ -19,6 +23,7 @@ public class Product {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @NotBlank(message = "Product name is required")
     @Column(nullable = false)
     private String name;
 
@@ -26,9 +31,13 @@ public class Product {
 
     private String category;
 
+    @NotNull(message = "Price is required")
+    @DecimalMin(value = "0.0", inclusive = false, message = "Product price must be greater then 0")
     @Column(nullable = false)
     private BigDecimal price;
 
+    @NotNull(message = "Stock Quantity is required")
+    @Min(value = 0, message = "Stock can not be less then 0")
     @Column(name = "stock_quantity", nullable = false)
     private Integer stockQuantity;
 
